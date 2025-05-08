180dbeb4871332f757e9410c3630c92d7955efdc
Bhavye Mathur <bhavyemathur@gmail.com>
2025-05-08 11:04:55 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hopfield Network in C++
+
+| ![](models/food.png) | ![](output/donut.gif)  | ![](output/burger.gif) |
+|----------------------|------------------------|------------------------|
--- gif_generator.py
+++ gif_generator.py
@@ -2,10 +2,10 @@ from PIL import Image
 import os
 
 input_folder = "inference"
-output_file = "output.gif"
+output_file = "output/output.gif"
 fps = 30
 
-images = sorted([img for img in os.listdir(input_folder)], key=lambda x: int(x[4:-4]))
+images = sorted([img for img in os.listdir(input_folder)], key=lambda x: int(x[6:-4]))
 
 frames = []
 for image in images:
@@ -14,5 +14,5 @@ for image in images:
     frame = frame.resize((512, 512), Image.NEAREST)
     frames.append(frame)
 
-frames[0].save(output_file, save_all=True, append_images=frames[1:], duration=1000 // fps, loop=True, optimize=True)
+frames[0].save(output_file, save_all=True, append_images=frames[1:], duration=1000 // fps, loop=0, optimize=True)
 print(f"GIF saved as {output_file}")
