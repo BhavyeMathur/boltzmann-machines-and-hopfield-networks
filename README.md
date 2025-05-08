cac2556f3501a7ab13225b85e4b69931a779ace5
Bhavye Mathur <bhavyemathur@gmail.com>
2025-05-08 12:19:25 -0500

    Updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 | ![](models/food.png) | ![](output/donut.gif)  | ![](output/burger.gif) |
 |----------------------|------------------------|------------------------|
+
+A simple C++ implementation of a Hopfield Network with Hebbian learning rules.
+
+### Mathematical Details
+
+We structure $n$ images with $p$ pixels each into an $n\times p$ matrix called $M$ (memory).
+
+Then, we initialize the network weights given by $W\in\mathbb{R}^{p\times p}$ where,
+
+$$W = \frac{1}{n}M^\top M$$
+
+To run inference, we initialize a state vector $\vec{s}\in\mathbb{R}^p$ with random integer entries between -2 and 2. Finally, we iteratively update random entries of $\vec{s}$ according to the following rule,
+
+$$s_i= \begin{cases}
+1, &         \vec{s}\cdot W_i \ge0,\\
+-1, &         \text{otherwise}
+\end{cases}$$
--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <string>
 
-#include <Eigen/Dense>
-#include "cnpy.h"
-
 #include "HopfieldNetwork.h"
 
 using namespace std;
@@ -14,21 +11,15 @@ int main() {
 //    HopfieldNetwork model(data);
 //    model.save("models/mnist.txt");
 //    model.save_png("models/mnist.png");
+//    HopfieldNetwork model("models/mnist.txt");  // load model
 
     string path = "data/food/food.npy";
     auto data = (read_npy_file(path) * 2).array() - 1;
-
-//    auto output = 255 * (data.reshaped(24, 24) + 1);
-//    write_matrix_to_png(output, "test.png");
-
     HopfieldNetwork model(data);
     model.save("models/food.txt");
     model.save_png("models/food.png");
 
-//    HopfieldNetwork model("models/mnist.txt");
-
     model.randomize_state();
-
     for (int i = 0; i < 150; i++) {
         model.update_state(30);
         model.save_state(format("inference/output{}.png", i));
